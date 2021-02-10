6c301d6dd55f76776b8dc973f5849813f07ce343
JoseMPeTe <jmperezterrero@gmail.com>
2021-02-10 11:05:54 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
